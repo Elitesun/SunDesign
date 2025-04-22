4be498ca74311b35c9753a2c24426c31820805af
Elitesun <elitesun.dev@gmail.com>
2025-04-22 19:35:26 +0000

    last commit

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useState } from 'react'
 import Brand from './Brand';
 import ShinyText from './ui/ShinyText';
+import { useState } from 'react';
 
 const Navbar = () => {
-    const [state, setState] = useState(false)
+    const [isNavOpen, setIsNavOpen] = useState(false);
 
     const navigation = [
         { title: "Home", path: "#home" },
@@ -12,46 +12,36 @@ const Navbar = () => {
         { title: "Footer", path: "#footer" },
     ]
 
-    const handleNavMenu = () => {
-        setState(!state)
-        document.body.classList.toggle("overflow-hidden")
-    }
-
     const handleSmoothScroll = (e: React.MouseEvent<HTMLAnchorElement>, path: string) => {
         e.preventDefault();
         const element = document.querySelector(path);
         if (element) {
             element.scrollIntoView({ behavior: "smooth" });
-            setState(false);
             document.body.classList.remove("overflow-hidden");
+            setIsNavOpen(false);
         }
     }
 
     return (
         <header>
-            <nav className={` w-full md:static md:text-sm ${state ? "fixed z-10 h-full" : ""}`}>
+            <nav className="w-full md:static md:text-sm">
                 <div className="custom-screen items-center mx-auto md:flex">
                     <div className="flex items-center justify-between py-3 md:py-5 md:block">
                         <Brand />
-                        <div className="md:hidden">
-                            <button role="button" aria-label="Open the menu" className="text-gray-500 hover:text-gray-800"
-                                onClick={handleNavMenu}
-                            >
-                                {
-                                    state ? (
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                                        </svg>
-                                    ) : (
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                                        </svg>
-                                    )
-                                }
-                            </button>
-                        </div>
+                        <button
+                            className="md:hidden"
+                            onClick={() => setIsNavOpen(!isNavOpen)}
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                {isNavOpen ? (
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                                ) : (
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                                )}
+                            </svg>
+                        </button>
                     </div>
-                    <div className={`flex-1 pb-3 mt-8 md:pb-0 md:mt-0 md:block ${state ? "" : "hidden"}`}>
+                    <div className={`${isNavOpen ? 'block' : 'hidden'} flex-1 pb-3 mt-8 md:pb-0 md:mt-0 md:flex md:items-center md:justify-end`}>
                         <ul className="text-gray-700 justify-end items-center space-y-6 md:flex md:space-x-6 md:space-y-0 md:text-gray-600 md:font-medium">
                             {
                                 navigation.map((item, idx) => {
@@ -82,4 +72,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar;
