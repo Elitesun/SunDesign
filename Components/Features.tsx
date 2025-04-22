5d2ccac097322d7c5125ceca2105e620118cc7b6
Elitesun <elitesun.dev@gmail.com>
2025-04-22 15:24:27 +0000

    first commit . created every component

---
+++ Components/Features.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+const Features = () => {
+    return (
+        <section id="features" className="py-12 bg-gray-50">
+            <div className="custom-screen">
+                <div className="max-w-screen-xl mx-auto px-4 flex flex-col lg:flex-row items-center justify-center gap-x-12 gap-y-8">
+                    <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
+                        <div className="hover:scale-[1.1] transition duration-700 ease-in-out">
+                            <Image
+                                src="/cover.jpg"
+                                width={600}
+                                height={400}
+                                className="rounded-lg w-full max-w-md"
+                                alt="Create Successful Business Models with Our IT Solutions"
+                            />
+                        </div>
+                    </div>
+                    <div className="w-full lg:w-1/2 max-w-xl">
+                        <h2 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+                            Figma Design Kit
+                        </h2>
+                        <p className="mt-3 text-gray-600 text-lg">
+                            Get access to 100+ components, 15+ Landing pages & more in one
+                            design kit on Figma!
+                        </p>
+                        <div className="mt-4">
+                            <span className="text-xl">$29 (one-time)</span>
+                            <br />
+                            <span className="text-md font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500">
+                                SPECIAL 40% OFF{" "}
+                                <span className="line-through text-black">$49</span> $29
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Features;
---
+++ Components/Hero.tsx
@@ -0,0 +1,60 @@
+const Hero = () => (
+    <section id="home" className="relative">
+        <div className="custom-screen py-28 text-gray-600">
+            <div className="space-y-5 max-w-4xl mx-auto text-center">
+                <h1 className="text-4xl text-gray-800 font-extrabold mx-auto sm:text-6xl">
+                    <span className="relative">
+                        Well-Designed
+                        <span className="absolute inset-x-0 bottom-0 h-0.5 bg-gradient-to-r from-green-500 via-blue-500 to-purple-500"></span>
+                    </span>
+                    {" "}Landing Page{" "}
+                    Components that {" "}
+                    <span className="relative">
+                        Convert 🚀
+                        <span className="absolute inset-x-0 bottom-0 h-0.5 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"> </span>
+                    </span>
+                </h1>
+
+                <p className="max-w-xl mx-auto">
+                    Be a pro at designing landing pages that convert. Get access to our
+                    design kit with 100+ components, 15+ landing pages and 10+ full templates built in Figma.
+                </p>
+                <div className="flex items-center justify-center gap-x-3 font-medium text-sm">
+                    <span className="text-white text-bold text-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 mb-5 hover:from-yellow-500 hover:via-red-500 hover:to-pink-500 hover:ring ring-transparent ring-offset-2 transition">
+                        $29 (one-time)
+                    </span>
+                </div>
+                <span className="text-md font-bold bg-clip-text text-transparent bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 ">
+                    SPECIAL 40% OFF <span className="line-through text-black">$49</span>   $29
+                </span>
+                <h2 className="font-semibold text-sm text-gray-600 text-center mb-4">
+                    JOIN 430+ USERS NOW
+                </h2>
+                <div className="flex flex-wrap items-center justify-center">
+                    <img
+                        className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+                        src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                    />
+                    <img
+                        className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+                        src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                    />
+                    <img
+                        className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+                        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+                        alt=""
+                    />
+                    <img
+                        className="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        alt=""
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+);
+
+export default Hero;
